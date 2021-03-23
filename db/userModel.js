51fc3bb34b170c4c314097ed8bcb451a9440773b
Joshua Lokken <jrrlokken@gmail.com>
2021-03-23 13:37:43 -0500

    Add dbConnect function and simple user model

---
+++ db/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please provide an email address'],
+    unique: [true, 'Email already registered'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    unique: false,
+  },
+});
+
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
